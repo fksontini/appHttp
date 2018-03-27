52f5a5ae59cb873072a9d4cfbdc8714fe8261cd8
fksontini <Fir@sino2019>
2018-03-27 18:58:29 +0100

    all

--- CLI/httpApp/src/app/post/post.component.ts
+++ CLI/httpApp/src/app/post/post.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
 import { Http } from '@angular/http';
 
-@Component({
+@Component
+({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
 export class PostComponent  {
 tabPosts;
-  constructor(http:Http) {
-http.get("https://jsonplaceholder.typicode.com/posts").subscribe(resp=>{
-console.log(resp.json())
-this.tabPosts=resp.json();
-});
+  constructor(http:Http) 
+    {
+      http.get("https://jsonplaceholder.typicode.com/posts").subscribe(resp=>
+      {
+      console.log(resp.json())
+      this.tabPosts=resp.json();
+     });
    }
 
 }
